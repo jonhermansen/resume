e47379f7c40bf6ca30d53fff01b1f91aaad55843
Jon Hermansen <jon@jh86.org>
2025-07-16 23:41:12 -0400

    First pass at getting things going with Nix

---
+++ README.md
@@ -0,0 +1,19 @@
+# jonhermansen-resume
+
+A simple resume generator tool.
+
+## Installation
+
+```bash
+cargo install jonhermansen-resume
+```
+
+## Usage
+
+```bash
+jonhermansen-resume
+```
+
+## License
+
+MIT
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Jon Hermansen's resume generator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rustfmt" "clippy" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            openssl
+          ];
+          
+          shellHook = ''
+            echo "Welcome to jonhermansen-resume development environment!"
+            echo "Rust version: $(rustc --version)"
+            echo "Cargo version: $(cargo --version)"
+          '';
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "jonhermansen-resume";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+        };
+      });
+}
