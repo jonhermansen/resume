68fe117601781e00f67da341074f0422da47b0d9
Jon Hermansen <jon@jh86.org>
2025-10-07 04:25:09 +0000

    Add fonts

--- flake.nix
+++ flake.nix
@@ -3,112 +3,90 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     typix = {
       url = "github:loqusion/typix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     flake-utils.url = "github:numtide/flake-utils";
-
-    # Example of downloading icons from a non-flake source
-    # font-awesome = {
-    #   url = "github:FortAwesome/Font-Awesome";
-    #   flake = false;
-    # };
   };
 
-  outputs = inputs @ {
-    nixpkgs,
-    typix,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (pkgs) lib;
-
-      typixLib = typix.lib.${system};
-
-      src = typixLib.cleanTypstSource ./.;
-      commonArgs = {
-        typstSource = "resume.typ";
-
-        fontPaths = [
-          # Add paths to fonts here
-          # "${pkgs.roboto}/share/fonts/truetype"
-        ];
-
-        virtualPaths = [
-          # Add paths that must be locally accessible to typst here
-          # {
-          #   dest = "icons";
-          #   src = "${inputs.font-awesome}/svgs/regular";
-          # }
-        ];
-      };
-
-      unstable_typstPackages = [
-        {
-          name = "modern-cv";
-          version = "0.9.0";
-          hash = "sha256-zCv2UABp3lBBbYthrSXD4OXWaiIjMdwRPQbq5a8AlUk=";
-        }
-        {
-          name = "fontawesome";
-          version = "0.6.0";
-          hash = "sha256-dgb+YAYLEKgMMEWa8yelMvRdEoesPj5HI+70w3mCUcQ=";
-        }
-        {
-          name = "linguify";
-          version = "0.4.2";
-          hash = "sha256-kuoK0r29kvc0rvDIQWELp/fZUm3Bzxc5W8M/YMU3lvg=";
-        }
-      ];
-
-      # Compile a Typst project, *without* copying the result
-      # to the current directory
-      build-drv = typixLib.buildTypstProject (commonArgs
-        // {
-          inherit src unstable_typstPackages;
-        });
-
-      # Compile a Typst project, and then copy the result
-      # to the current directory
-      build-script = typixLib.buildTypstProjectLocal (commonArgs
-        // {
-          inherit src unstable_typstPackages;
-        });
-
-      # Watch a project and recompile on changes
-      watch-script = typixLib.watchTypstProject commonArgs;
-    in {
-      checks = {
-        inherit build-drv build-script watch-script;
-      };
-
-      packages.default = build-drv;
-
-      apps = rec {
-        default = watch;
-        build = flake-utils.lib.mkApp {
-          drv = build-script;
+  outputs =
+    inputs@{
+      nixpkgs,
+      typix,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        typixLib = typix.lib.${system};
+        src = typixLib.cleanTypstSource ./.;
+        commonArgs = {
+          typstSource = "resume.typ";
+          fontPaths = [
+            "${pkgs.roboto}/share/fonts/truetype"
+            "${pkgs.source-sans-pro}/share/fonts/truetype"
+          ];
+          virtualPaths = [ ];
         };
-        watch = flake-utils.lib.mkApp {
-          drv = watch-script;
-        };
-      };
-
-      devShells.default = typixLib.devShell {
-        inherit (commonArgs) fontPaths virtualPaths;
-        packages = [
-          # WARNING: Don't run `typst-build` directly, instead use `nix run .#build`
-          # See https://github.com/loqusion/typix/issues/2
-          # build-script
-          watch-script
-          # More packages can be added here, like typstfmt
-          # pkgs.typstfmt
+        unstable_typstPackages = [
+          {
+            name = "modern-cv";
+            version = "0.9.0";
+            hash = "sha256-zCv2UABp3lBBbYthrSXD4OXWaiIjMdwRPQbq5a8AlUk=";
+          }
+          {
+            name = "fontawesome";
+            version = "0.6.0";
+            hash = "sha256-dgb+YAYLEKgMMEWa8yelMvRdEoesPj5HI+70w3mCUcQ=";
+          }
+          {
+            name = "linguify";
+            version = "0.4.2";
+            hash = "sha256-kuoK0r29kvc0rvDIQWELp/fZUm3Bzxc5W8M/YMU3lvg=";
+          }
         ];
-      };
-    });
+        build-drv = typixLib.buildTypstProject (
+          commonArgs
+          // {
+            inherit src;
+            inherit unstable_typstPackages;
+          }
+        );
+        build-script = typixLib.buildTypstProjectLocal (
+          commonArgs
+          // {
+            inherit src;
+            inherit unstable_typstPackages;
+          }
+        );
+        watch-script = typixLib.watchTypstProject commonArgs;
+      in
+      {
+        checks = {
+          inherit build-drv;
+          inherit build-script;
+          inherit watch-script;
+        };
+        packages.default = build-drv;
+        apps = rec {
+          default = watch;
+          build = flake-utils.lib.mkApp {
+            drv = build-script;
+          };
+          watch = flake-utils.lib.mkApp {
+            drv = watch-script;
+          };
+        };
+        devShells.default = typixLib.devShell {
+          inherit (commonArgs) fontPaths;
+          inherit (commonArgs) virtualPaths;
+          packages = [
+            watch-script
+          ];
+        };
+      }
+    );
 }
